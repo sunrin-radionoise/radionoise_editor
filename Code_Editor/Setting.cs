c0837ea5f8b6ef82fcc01da8405c275f7dd079f5
YonghoAhn <hrh1110@naver.com>
2017-01-08 09:20:37 +0900

    Settings Field Add

---
+++ Code_Editor/Setting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Editor
+{
+    class Setting
+    {
+        /* TODO : Add Setting Field
+         * Font
+         * Background Image
+         * Background Color
+         * Account
+         * 
+         */
+    }
+}
